6b75f3e1b4ba26d9aff431bebd9a64629302d512
Tom Davis <tom@tomdavislx.co.uk>
2017-01-18 15:17:29 +0000

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Tom Davis on 18/01/2017.
+//  Copyright © 2017 Tom Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
